faf9b156d01ce5c402ebd17d44423c987e116883
Nathan van Doorn <nvd1234@gmail.com>
2015-04-10 23:09:53 +0100

    Comment all the things!
    Need to make these haddocky at some point

--- NYHC/Record.hs
+++ NYHC/Record.hs
@@ -6,11 +6,18 @@ import Control.Monad.Trans.Writer
 import qualified Data.Map.Strict as M
 import Language.Haskell.Exts.Syntax
 
+-- given a declaration of a data constructor, turns it into a plain one if it
+-- were previously a record
 reconstruct :: QualConDecl -> QualConDecl
 reconstruct (QualConDecl sl bs cxt c) = QualConDecl sl bs cxt $ case c of
   RecDecl name ts -> ConDecl name (map snd ts)
   _ -> c
 
+-- goes through a list of declerations and removes (but remembers) all the
+-- declerations for records, changing them into plain old product types
+-- returns a tuple of:
+-- * the declerations for data types containing records that have been removed
+-- * the altered list of declerations
 undefineRecords :: [Decl] -> ([Decl], [Decl])
 undefineRecords ds0 = runWriter $ forM ds0 $ \decl -> case decl of
   DataDecl sl don con name ts qs ds ->
@@ -22,9 +29,21 @@ undefineRecords ds0 = runWriter $ forM ds0 $ \decl -> case decl of
       return decl
   _ -> return decl
 
+-- utility function to reshuffle a list a bit
+-- I use it because sometimes multiple record fields share the same type decleration
+-- so this spreads those out
 reshuffle :: [([a], b)] -> [(a, b)]
 reshuffle = concatMap (\(ns, t) -> map (flip (,) t) ns)
 
+-- take the decleration of a datatype, and return a tuple of:
+-- * the location of the datatype in the file
+-- * the name of the datatype
+-- * a map from names of record fields defined for the data type and a tuple of their type and a list of:
+-- * * a constructor it is defined for
+-- * * its position in the arguments of the constructor
+-- * * the number of arguments the constructor has
+--
+-- for declarations that do not define a datatype, this function is undefined.
 getAccessorInformation :: Decl -> (SrcLoc, Name, M.Map Name (Type, [(Name,Int,Int)]))
 getAccessorInformation (DataDecl sl _ _ name _ qs _) = 
   let recs = [(n, reshuffle fs) | (QualConDecl _ _ _ (RecDecl n fs)) <- qs]
@@ -34,6 +53,9 @@ getAccessorInformation (DataDecl sl _ _ name _ qs _) =
     mergeThings (t1, xs) (t2, ys) = assert (t1 == t2) (t1, xs ++ ys)
 getAccessorInformation _ = error "stuff"
 
+-- goes through the list of declerations of record data types and returns a list
+-- of declerations for functions that match up with the previously-defined
+-- field accessors
 redefineAccessors :: [Decl] -> [Decl]
 redefineAccessors decls = do
   (sl, name, m) <- map getAccessorInformation decls
